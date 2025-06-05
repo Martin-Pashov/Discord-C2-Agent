9da42bbd7d65675e06efa48d4430737f798df559
Martin <martins124124@gmail.com>
2025-06-05 02:47:15 +0200

    Update bot.py
    Experimenting with command execution. Output displaying added.

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 import asyncio
+import os
+import subprocess
 
 TOKEN = ''     # Replace with your bot's token
 CHANNEL_ID = 1379950289569976330 # Replace with your channel ID
@@ -14,11 +16,20 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
+    cmd = message.content
     
     if message.author == client.user:
         return
     
-    if message.channel.id == CHANNEL_ID:
-        await message.channel.send(message.content)
+    result = subprocess.run(['type', 'C:\\Users\\marti\\Desktop\\Important Info.txt'], capture_output=True, text=True, shell=True)
+    output = result.stdout.strip()    
+    
+    for i in range(0, len(output), 1990):
+        
+        if i == 0:
+            await message.channel.send(f"{cmd}:")
+
+        chunk = output[i:i+1990]
+        await message.channel.send(f"```{chunk}```")
     
 client.run(TOKEN)
